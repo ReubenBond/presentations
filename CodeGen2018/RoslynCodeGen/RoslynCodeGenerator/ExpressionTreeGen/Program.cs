885eedc0633b78d90532e44aa407ad463fc62afd
ReubenBond <a-rebond@microsoft.com>
2018-04-23 21:14:25 +1000

    Initial commit

---
+++ CodeGen2018/RoslynCodeGen/RoslynCodeGenerator/ExpressionTreeGen/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+        using System;
+        using System.Linq.Expressions;
+
+        namespace ExpressionTreeGen
+        {
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    var paramA = Expression.Parameter(typeof(int), "a");
+                    var paramB = Expression.Parameter(typeof(int), "b");
+
+                    var expr = Expression.Lambda<Func<int, int, int>>(
+                        body: Expression.Add(paramA, paramB),
+                        parameters: new[] {paramA, paramB});
+
+                    var compiled = expr.Compile();
+
+                    var sum = compiled(1, 2);
+
+                    Console.WriteLine(sum);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+
