0745dc9d30a00687d0a1bd8b8f020bf3e139d054
ReubenBond <a-rebond@microsoft.com>
2018-04-24 11:55:10 +1000

    2

---
+++ CodeGen2018/RoslynCodeGen/EmitIL/Program.cs
@@ -0,0 +1,27 @@
+﻿    using System;
+    using System.Reflection.Emit;
+
+    namespace EmitIL
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var method = new DynamicMethod(
+                    name: "add",
+                    returnType: typeof(int),
+                    parameterTypes: new[] {typeof(int), typeof(int)});
+                var il = method.GetILGenerator();
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Ldarg_1);
+                il.Emit(OpCodes.Add);
+                il.Emit(OpCodes.Ret);
+
+                var add = (Func<int, int, int>)method.CreateDelegate(typeof(Func<int, int, int>));
+
+                var sum = add(1, 2);
+                Console.WriteLine(sum);
+                Console.ReadKey();
+            }
+        }
+    }
