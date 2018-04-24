bbdaf746015c0bfe8523f4c71daef22cd14ec298
Reuben Bond <a-rebond@microsoft.com>
2018-04-24 14:55:47 +1000

    update

---
+++ CodeGen2018/Examples/RoslynCodeGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace RoslynCodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var syntax = CompilationUnit()
+                .WithMembers(
+                    SingletonList<MemberDeclarationSyntax>(
+                        NamespaceDeclaration(ParseName("Powerful"))
+                            .WithMembers(
+                                SingletonList<MemberDeclarationSyntax>(
+                                    ClassDeclaration("Yeah")
+                                        .WithMembers(
+                                            SingletonList<MemberDeclarationSyntax>(
+                                                MethodDeclaration(ParseTypeName("void"), "PowderedSugar")
+                                                    .WithBody(Block())))))));
+            syntax = syntax.NormalizeWhitespace();
+            Console.WriteLine(syntax.ToFullString());
+            
+            Console.ReadLine();
+        }
+    }
+}
+
+
